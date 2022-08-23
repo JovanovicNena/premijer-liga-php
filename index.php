741bec5d69a14cf992120743cfc4d53a7bdf49c8
JovanovicNena <jovanovicnenad320@gmail.com>
2022-08-23 19:48:11 +0200

    tabela liste strelaca iz baze - index.php

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Premijer liga - Lista strelaca</title>
+</head>
+
+<body>
+
+    <div class="main">
+
+        <table class="tabela-igraci">
+
+            <thead>
+                <tr>
+                    <th>Ime</th>
+                    <th>Prezime</th>
+                    <th>Godine</th>
+                    <th>Broj na dresu</th>
+                    <th>Klub</th>
+                    <th>Broj golova</th>
+                </tr>
+            </thead>
+
+            <?php
+            $db_connection = new mysqli("localhost", "root", "", "premijerliga");
+            $query = "SELECT fudbaler.id, fudbaler.ime, fudbaler.prezime, fudbaler.godine, fudbaler.broj_dres, klub.ime as kime, fudbaler.broj_golova FROM fudbaler JOIN klub ON fudbaler.klub_id = klub.id";
+            $data = $db_connection->query($query);
+
+            while ($row = mysqli_fetch_array($data)) :
+            ?>
+                <tbody>
+                    <tr>
+                        <td><?php echo $row['ime'];  ?></td>
+                        <td><?php echo $row['prezime'];  ?></td>
+                        <td><?php echo $row['godine'];  ?></td>
+                        <td><?php echo $row['broj_dres']; ?></td>
+                        <td><?php echo $row['kime']; ?></td>
+                        <td><?php echo $row['broj_golova']; ?></td>
+                    </tr>
+                <?php
+            endwhile;
+                ?>
+                </tbody>
+
+        </table>
+
+    </div>
+
+</body>
+
+</html>
